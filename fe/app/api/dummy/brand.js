47230d7cd7c763d2e2048f4cf7340fd0d699adb4
hai <hainvhth2108031@fpt.edu.vn>
2023-09-29 19:28:28 +0700

    initial fe frameworks'

---
+++ fe/app/api/dummy/brand.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'User Management',
+  desc: 'User Management System',
+  prefix: 'enlite',
+  footerText: 'User Management All Rights Reserved 2020',
+  logoText: 'User Management',
+};
---
+++ fe/app/api/ui/menu.js
@@ -0,0 +1,84 @@
+module.exports = [
+  {
+    key: "home_page",
+    name: "Home",
+    icon: "home",
+    link: "/app/pages/home",
+  },
+  {
+    key: "mange_user",
+    name: "Manage Users",
+    icon: "account_box",
+    child: [
+      {
+        key: "user_list",
+        name: "User List",
+        icon: "view_list",
+        link: "/app/pages/user",
+      },
+      {
+        key: "user_add",
+        name: "Add User",
+        icon: "add_box",
+        link: "/app/pages/add/user",
+      },
+      {
+        key: "user_import",
+        name: "Import User",
+        icon: "file_upload",
+        link: "/app/pages/import/user",
+      },
+    ],
+  },
+  {
+    key: "manage_leave",
+    name: "Manage Leaves",
+    icon: "view_stream",
+    link: "/app/pages/leave/manage",
+  },
+  {
+    key: "apply_leave",
+    name: "Apply Leave",
+    icon: "view_list",
+    link: "/app/pages/leave/apply",
+  },
+  {
+    key: "my_leave",
+    name: "My Leaves",
+    icon: "view_list",
+    link: "/app/pages/leave/my",
+  },
+
+  {
+    key: "pages",
+    name: "Pages",
+    icon: "important_devices",
+    child: [
+      {
+        key: "blank",
+        name: "Blank Page",
+        icon: "video_label",
+        link: "/app",
+      },
+
+      // {
+      //   key: 'dashboard',
+      //   name: 'Dashboard',
+      //   icon: 'settings_brightness',
+      //   link: '/app/pages/dashboard'
+      // },
+      {
+        key: "forms",
+        name: "Form",
+        link: "/app/pages/form",
+        icon: "ballot",
+      },
+      {
+        key: "tables",
+        name: "Table",
+        icon: "grid_on",
+        link: "/app/pages/table",
+      },
+    ],
+  },
+];
