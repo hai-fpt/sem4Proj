3b50e4900a3a47a307db478fd84adc9fa033f9d2
tiennvth2108051 <96479529+tiennvth2108051@users.noreply.github.com>
2023-10-23 16:12:44 +0700

    update Department

--- fe/app/containers/Pages/Department/index.js
+++ fe/app/containers/Pages/Department/index.js
@@ -21,17 +21,20 @@ import {
   postDepartment,
   deleteDepartment,
   fetchManager,
-  fetchDepartment
+  fetchDepartment, fetchTeam
 } from "../../../api/department/department";
-import {setManager} from "../../../redux/actions/departmentActions";
+import {setManager, setTeam} from "../../../redux/actions/departmentActions";
+import {injectIntl} from 'react-intl';
+import messages from "enl-api/department/departmentMessages";
 
 
-const Department = () => {
+const Department = ({intl}) => {
   const [reloadKey, setReloadKey] = useState(0);
   const [tabValue, setTabValue] = useState(0);
   const [formData, setFormData] = useState({
     name: '',
     managerId: 0,
+    teamList: [],
   })
   const [detailFormData, setDetailFormData] = useState();
   const [forceRender, setForceRender] = useState(false);
@@ -39,6 +42,8 @@ const Department = () => {
   const tabItems = departmentService.getTabItems(detailFormData);
   const [data, setData] = useState([]);
   const dispatch = useDispatch();
+  const [notificationMessage, setNotificationMessage] = useState('');
+  const [notificationSeverity, setNotificationSeverity] = useState('');
 
   const handleTabValueProps = (propsFromChild) => {
     setTabValue(propsFromChild)
@@ -55,14 +60,14 @@ const Department = () => {
     departmentService.handleEditProcessing(item, handleTabValueProps, setDetailFormData, setFormData);
   }
 
-  const handleDepartmentFormSubmit = (e) => {
+  const handleDepartmentFormSubmit = async (e) => {
     e.preventDefault();
     const { id } = formData;
     if (id === undefined) {
-      handlePostDepartment();
+      await handlePostDepartment();
     }
     if (id !== undefined) {
-      handlePutDepartment();
+      await handlePutDepartment();
     }
   }
 
@@ -71,22 +76,19 @@ const Department = () => {
     const body = {
       name: formData.name,
       managerId: formData.manager,
+      teamList: formData.team,
     };
-    await putDepartment(baseApiUrl, id, body);
-    setOpenNotification(true);
-    setReloadKey((prevCount) => prevCount + 1);
-    handleTabValueProps(0);
+    await putDepartment(baseApiUrl, id, body, setOpenNotification, setNotificationMessage, setNotificationSeverity, handleTabValueProps, intl);
   };
 
   const handlePostDepartment = async () => {
+    console.log('formData', formData)
     const body = {
       name: formData.name,
       managerId: formData.manager,
+      teamList: formData.team,
     };
-    await postDepartment(baseApiUrl, body);
-    setOpenNotification(true);
-    setReloadKey((prevCount) => prevCount + 1);
-    handleTabValueProps(0);
+    await postDepartment(baseApiUrl, body, setOpenNotification, setNotificationMessage, setNotificationSeverity, handleTabValueProps, intl);
   };
   const handleCancelEdit = (e) => {
     departmentService.handleCancelEdit(e,
@@ -100,14 +102,14 @@ const Department = () => {
 
   const handleDeleteProcessing = async (value) => {
     const id = value[0]
-    await deleteDepartment(baseApiUrl, id)
+    await deleteDepartment(baseApiUrl, id, setOpenNotification, setNotificationMessage, setNotificationSeverity, setReloadKey, intl)
   }
 
   const baseApiUrl = useSelector(state => state.env.BASE_API_URL);
 
-  useEffect(async () => { setData(await fetchDepartment(baseApiUrl))}, [formData, tabValue, detailFormData]);
+  useEffect(async () => { setData(await fetchDepartment(baseApiUrl))}, [tabValue, reloadKey]);
 
-  useEffect(async () => { dispatch(setManager(await fetchManager(baseApiUrl)))}, [] )
+  useEffect(async () => { dispatch(setManager(await fetchManager(baseApiUrl))); dispatch(setTeam(await fetchTeam(baseApiUrl)))}, [])
 
   //data table setup
   const columns = [
@@ -121,13 +123,34 @@ const Department = () => {
     },
     {
       name: "name",
+      label: intl.formatMessage(messages.name),
       options: {
         filter: true,
         ...TableOptionStyle(),
       }
     },
     {
+      name: 'teams',
+      label: intl.formatMessage(messages.team),
+      options: {
+        filter: true,
+        ...TableOptionStyle(),
+        customBodyRender: (value) => {
+          if (Array.isArray(value)) {
+            return value.map((item, index) => (
+                <React.Fragment key={index}>
+                  {item.teamName}
+                  {index !== value.length - 1 && ', '}
+                </React.Fragment>
+            ));
+          }
+          return value;
+        },
+      }
+    },
+    {
       name: 'manager',
+      label: intl.formatMessage(messages.manager),
       options: {
         filter: true,
         ...TableOptionStyle(),
@@ -138,6 +161,7 @@ const Department = () => {
     },
     {
       name: 'createdDate',
+      label: intl.formatMessage(messages.created),
       options: {
         filter: true,
         ...TableOptionStyle(),
@@ -148,6 +172,7 @@ const Department = () => {
     },
     {
       name: 'Action',
+      label: intl.formatMessage(messages.action),
       options: {
         download: false,
         csv: false,
@@ -156,22 +181,10 @@ const Department = () => {
           return (
               <DataTableDropdownMenu delete edit ItemValue={rowValue} editProcessing={handleEditProcessing} deleteProcessing={handleDeleteProcessing}></DataTableDropdownMenu>
           )
-        },
+        }
       }
     },
   ];
-  // const data = [
-  //   ['Gio to Hung Vuong', 'Wed Jun 4 2023 14:18:15 GMT+0700 (Indochina Time)', 'Wed Jun 5 2023 14:18:15 GMT+0700 (Indochina Time)', 'The Hùng Kings Temple Festival (Vietnamese: Giỗ Tổ Hùng Vương or Lễ hội đền Hùng) is a Vietnamese festival held annually from the 8th to the 11th day of the third lunar month in honour of the'],
-  //   ['Giang Sinh', 'Wed Jun 4 2023 14:18:15 GMT+0700 (Indochina Time)' , 'Wed Jun 5 2023 14:18:15 GMT+0700 (Indochina Time)', 'Christmas is an annual festival commemorating the birth of Jesus Christ, observed primarily on December 25[a] as a religious and cultural celebration among billions of people around the world'],
-  //   ['Tet co truyen', 'Wed Jun 4 2023 14:18:15 GMT+0700 (Indochina Time)' , 'Wed Jun 5 2023 14:18:15 GMT+0700 (Indochina Time)', ''],
-  //   ['Linh tinh', 'Wed Jun 4 2023 14:18:15 GMT+0700 (Indochina Time)' , 'Wed Jun 5 2023 14:18:15 GMT+0700 (Indochina Time)', ''],
-  //   ['Gio to Hung Vuong', 'Wed Jun 4 2023 14:18:15 GMT+0700 (Indochina Time)', 'Wed Jun 5 2023 14:18:15 GMT+0700 (Indochina Time)', 'The Hùng Kings Temple Festival (Vietnamese: Giỗ Tổ Hùng Vương or Lễ hội đền Hùng) is a Vietnamese festival held annually from the 8th to the 11th day of the third lunar month in honour of the'],
-  //   ['Giang Sinh', 'Wed Jun 4 2023 14:18:15 GMT+0700 (Indochina Time)' , 'Wed Jun 5 2023 14:18:15 GMT+0700 (Indochina Time)', 'Christmas is an annual festival commemorating the birth of Jesus Christ, observed primarily on December 25[a] as a religious and cultural celebration among billions of people around the world'],
-  //   ['Gio to Hung Vuong', 'Wed Jun 4 2023 14:18:15 GMT+0700 (Indochina Time)', 'Wed Jun 5 2023 14:18:15 GMT+0700 (Indochina Time)', 'The Hùng Kings Temple Festival (Vietnamese: Giỗ Tổ Hùng Vương or Lễ hội đền Hùng) is a Vietnamese festival held annually from the 8th to the 11th day of the third lunar month in honour of the'],
-  //   ['Giang Sinh', 'Wed Jun 4 2023 14:18:15 GMT+0700 (Indochina Time)' , 'Wed Jun 5 2023 14:18:15 GMT+0700 (Indochina Time)', 'Christmas is an annual festival commemorating the birth of Jesus Christ, observed primarily on December 25[a] as a religious and cultural celebration among billions of people around the world'],
-  //   ['Gio to Hung Vuong', 'Wed Jun 4 2023 14:18:15 GMT+0700 (Indochina Time)', 'Wed Jun 4 2023 14:18:15 GMT+0700 (Indochina Time)', 'The Hùng Kings Temple Festival (Vietnamese: Giỗ Tổ Hùng Vương or Lễ hội đền Hùng) is a Vietnamese festival held annually from the 8th to the 11th day of the third lunar month in honour of the'],
-  //   ['Giang Sinh', 'Wed Jun 4 2023 14:18:15 GMT+0700 (Indochina Time)' , 'Wed Jun 5 2023 14:18:15 GMT+0700 (Indochina Time)', 'Christmas is an annual festival commemorating the birth of Jesus Christ, observed primarily on December 25[a] as a religious and cultural celebration among billions of people around the world'],
-  // ];
 
   return (
       <>
@@ -181,8 +194,8 @@ const Department = () => {
           </Helmet>
           <PapperBlock title="Department"
                        whiteBg
-                       icon="local_airport"
-                       desc="This module allows admins to view and update department."
+                       icon="account_balance"
+                       desc={intl.formatMessage(messages.title)}
           >
             <TabsNavigation tabItems={tabItems} tabValuePropsFromChild={handleTabValueProps} tabValuePropsFromParent={tabValue}></TabsNavigation>
           </PapperBlock>
@@ -194,15 +207,16 @@ const Department = () => {
                     columns={columns}
                     options={TableOptionsSetup}
                 />
-                 {/*<div>Total: &nbsp; {data.length}</div> */}
+                 <div>Total: &nbsp; {data.length}</div>
               </Box>
             </TabPanel>
 
             <TabPanel tabIndex={1} tabValue={tabValue} forceRender={forceRender}>
               <Box p={3}>
                 <form onSubmit={handleDepartmentFormSubmit}>
-                  <CustomFormElement detailFormData={detailFormData} valueFormProps={handleFormValueProps} field={'name'} label={'Name'} isRequired formElementType='text' />
-                  <CustomFormElement detailFormData={detailFormData} valueFormProps={handleFormValueProps} field={'manager'} label={'Manager'} isRequired formElementType={'department_manager_select'} />
+                  <CustomFormElement detailFormData={detailFormData} valueFormProps={handleFormValueProps} field={'name'} label={intl.formatMessage(messages.name)} isRequired formElementType='text' />
+                  <CustomFormElement detailFormData={detailFormData} valueFormProps={handleFormValueProps} field={'manager'} label={intl.formatMessage(messages.manager)} isRequired formElementType={'department_manager_select'} />
+                  <CustomFormElement detailFormData={detailFormData} valueFormProps={handleFormValueProps} field={'team'} label={intl.formatMessage(messages.team)} isRequired formElementType={'department_multi_select_team'} />
                   <ButtonGroup detail={detailFormData} handleCancelEdit={handleCancelEdit}></ButtonGroup>
                 </form>
               </Box>
@@ -212,11 +226,11 @@ const Department = () => {
         <CustomNotification
             open={openNotification}
             close={() => { setOpenNotification(false) }}
-            notificationMessage={'Great! a department was created successfully.'}
-            severity={'success'}
+            notificationMessage={notificationMessage}
+            severity={notificationSeverity}
         />
       </>
   );
 }
 
-export default Department;
+export default injectIntl(Department);
